c4413855cc31ad6d915357037583c2e9606d3c70
Francisco Galuppo Azevedo <franciscogaluppo@dcc.ufmg.br>
2023-08-03 12:50:52 -0300

    minor newton change

--- src/newton.py
+++ src/newton.py
@@ -3,12 +3,11 @@ from tinygrad.tensor import Tensor
 import tinygrad.nn.optim as optim
 
 def jacobian(y, eq_resid):
-    y = Tensor(y.numpy()).cpu()
+    y = Tensor(y.numpy(), requires_grad=True).cpu()
     jac = np.zeros((len(eq_resid), *y.shape), np.float32)
     opt = optim.SGD([y],lr=1)
     for i, f in enumerate(eq_resid):
         opt.zero_grad()
-        
         f(y).backward()
         jac[i] = y.grad.numpy()
     return jac, np.linalg.inv(jac[:,-len(eq_resid):])
